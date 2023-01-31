ef79c484467182b49ffc32a444d1bf6657dc3a1d
Jacky Ko <jackkykokoko@gmail.com>
2023-01-31 18:53:54 +0000

    remove explicit macro

--- main.py
+++ main.py
@@ -4,39 +4,6 @@ import imagej
 import imagej.doctor
 import scyjava as sj
 
-# macro = """
-# import sys
-
-# from ij import IJ
-# from ij import WindowManager
-
-# from fiji.plugin.trackmate import Model, Logger
-
-# # We have to do the following to avoid errors with UTF8 chars generated in 
-# # TrackMate that will mess with our Fiji Jython.
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
-
-# # # Get currently selected image
-# # # imp = WindowManager.getCurrentImage()
-# imp = IJ.openImage('https://fiji.sc/samples/FakeTracks.tif')
-# imp.show()
-
-# #----------------------------
-# # Create the model object now
-# #----------------------------
-
-# # Some of the parameters we configure below need to have
-# # a reference to the model at creation. So we create an
-# # empty model now.
-
-# model = Model()
-
-# # Send all messages to ImageJ log window.
-# model.setLogger(Logger.IJ_LOGGER)
-# """
-
-
 def main():
     # export JAVA_HOME path for the virtual environment
     # if JAVA_HOME is not set and we are in a conda environment
