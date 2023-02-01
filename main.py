0ad1e8365cb84f18eb62c8fd02ad183b5a6afb3a
Jacky Ko <jackkykokoko@gmail.com>
2023-02-01 17:20:23 +0000

    headless mode ok

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import os
 import imagej
 import imagej.doctor
 import scyjava as sj
+from skimage import io
 
 def main():
+    # The local working path need to be stored as pyimagej will change the CWD after initialization
+    CWD = os.getcwd()
+
     # export JAVA_HOME path for the virtual environment
     # if JAVA_HOME is not set and we are in a conda environment
     if (not os.environ.get('JAVA_HOME')) and os.environ.get('CONDA_DEFAULT_ENV'):
@@ -16,40 +20,31 @@ def main():
     FIJI_DIR = "C:/Users/kko/Software/Fiji.app"
     IMAGE_PATH = "I:/Group Fritzsche/Jacky/tcell/1To2CancerToTcell_ratio/C1_Nyeso1HCT116_1G4PrimaryPure_Icam_CxCl12_CellTrackerDeepRed_640nm_Tcells_T1-30.tif"
 
-    # ImageJ Macro has to be load before initialization
-    with open("test.ijm","r") as f:
-        macro_ijm = f.read()
-
-    with open("trackmate_test.py","r") as f:
-        macro_py = f.read()
-
     print("Initializing Fiji on JVM...")
     ij = imagej.init(FIJI_DIR,mode='headless')
     # ij = imagej.init(FIJI_DIR,mode='interactive')
     print(ij.getApp().getInfo(True))
 
-    print("ImageJ loading ...")
-    # im = ij.io().open(IMAGE_PATH)
-    # im = ij.io().open('https://samples.fiji.sc/FakeTracks.tif')
-    # imp = ij.py.to_imageplus(im)
-    # ij.py.window_manager().setTempCurrentImage(imp)
-    # print("Starting TrackMate GUI...")
-    # ij.py.run_plugin('TrackMate', {})
+    print("Image loading on python side...")
+    image = io.imread(IMAGE_PATH)
+    imp = ij.py.to_imageplus(image)
 
-    # import TrackMate via scyjava
-    # TrackMate = sj.jimport('fiji.plugin.trackmate.TrackMate')
+    File = sj.jimport('java.io.File')
+    jfile = File(os.path.join(CWD,'trackmate.py'))
 
-    # loading fiji script from file
+    # convert python side stuffs to java side
+    # define python side arguments
     args = {
-        'name': 'Chuckles',
-        'age': 13,
-        'city': 'nowhere'
+        'imp': imp # java side ImagePlus object
     }
+    jargs = ij.py.jargs(args)
+    result_future = ij.script().run(jfile,True,jargs)
 
-    task = ij.py.run_script('ijm', macro_ijm, args)
-    task = ij.py.run_script('python', macro_py, args)
-
-    print(task.getOutput('greeting'))
+    # get the result from java future, blocking will occur here
+    result = result_future.get()
+    print(ij.py.from_java(result.getOutput("foo")))
+    print(ij.py.from_java(result.getOutput("bar")))
+    print(ij.py.from_java(result.getOutput("shape")))
     
 if __name__=="__main__":
     main()
